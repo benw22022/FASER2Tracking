6b22dbae3d319c4487bb8074389dd600f92a7906
Ben Wilson <benjamin.james.wilson@cern.ch>
2025-05-13 14:36:58 +0100

    add LCG release setup to setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # This script sets up the environment for FASER2 Python bindings.
 
-BUILD_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# Setup the LCG release
+source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc11-opt/setup.sh
 
+# Setup the python bindings
+BUILD_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source ${BUILD_DIR}/python/setup.sh                      # Sourcing this script sets up the Acts python bindings
 export PYTHONPATH=${BUILD_DIR}/lib64:$PYTHONPATH         # This adds our python bindings to the PYTHONPATH
 echo "INFO:    Setup FASER2 Python bindings complete."
